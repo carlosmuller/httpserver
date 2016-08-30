8621c0b49002fcc2472e4289f59b2dc4138feab7
Carlos Muller <kdu428@gmail.com>
2016-08-30 11:10:15 -0300

    Separando o request em uma classe nova, e dois métodos um para receber e um para enviar resposta. Ainda falta validar de acordo com a imagem

---
+++ httprequest.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+import logging
+import thread
+
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
+PACKET_LENGTH = 32
+
+
+class httprequest(object):
+    """Classe de processamento do http e da requisição"""
+
+    def __init__(self, conexao, cliente):
+        self.conexao = conexao
+        self.cliente = cliente[0]
+
+    def processarRequest(self):
+        msg = ""
+        while True:
+            tmp = self.conexao.recv(PACKET_LENGTH)
+            msg += tmp
+            if len(tmp) < PACKET_LENGTH:
+                break
+        self.sendResponse(msg)
+        logger.info('Cliente [%s] pediu %s com a mensagem completa:\n%s' % (self.cliente, msg.split('\r\n')[0], msg))
+        thread.exit()
+
+    def sendResponse(self, msg):
+        response_headers = {
+            'Content-Type': 'text/html; encoding=utf8',
+            'Content-Length': len(msg),
+            'Connection': 'close',
+        }
+        response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
+        response_proto = 'HTTP/1.1'
+        response_status = '200'
+        response_status_text = 'OK'
+        self.conexao.send('%s %s %s' % (response_proto, response_status, response_status_text))
+        self.conexao.send(response_headers_raw)
+        result = '\n<html><body>' + msg.replace('\r\n', '<br>') + '</body></html>'
+        self.conexao.send(result)
+        self.conexao.close()
