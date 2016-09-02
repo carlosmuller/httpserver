26269e6c96176980a5e15a861e26e98881d90c26
Carlos Muller <kdu428@gmail.com>
2016-09-01 21:53:55 -0300

    Código para a parte A. Falta documentação

--- httprequest.py
+++ httprequest.py
@@ -17,7 +17,7 @@ class httprequest(object):
 
     def __init__(self, conexao, cliente):
         self.conexao = conexao
-        self.cliente = cliente[0]
+        self.cliente = cliente
 
     def processarRequest(self):
         msg = ""
@@ -26,22 +26,32 @@ class httprequest(object):
             msg += tmp
             if len(tmp) < PACKET_LENGTH:
                 break
-        headerRequest = msg.split('\r\n')[0]
-        logger.info('Cliente [%s] pediu %s com a mensagem completa:\n%s' % (self.cliente, headerRequest, msg))
-        method = headerRequest.split(' ')[0].upper()
-        if method not in httpmethod.allmethods():
-            self.sendResponse("This method [%s] is not implemented yet" % method, httpstatus.status[501])
+        header = msg.split('\r\n')[0]
+        headerAtributes = header.split(' ')
+        logger.info('Cliente(ip, porta resposta ) [%s] pediu com o header [%s] com a mensagem completa:\n%s' % (self.cliente, header, msg))
+        if len(headerAtributes) != 3:
+            self.sendResponse("This is [%s] a invalid header for HTTP/1.0" % header, httpstatus.status[400])
         else:
-            self.sendResponse(msg,httpstatus.status[200])
+            method = headerAtributes[0].upper()
+            path = headerAtributes[1]
+            protocol = headerAtributes[2]
+            if not protocol.startswith("HTTP/1."):
+                self.sendResponse("Bad request don't accept protocol [%s]" % protocol, httpstatus.status[400])
+            else:
+                if method not in httpmethod.allmethods():
+                    self.sendResponse("This method [%s] is not implemented yet" % method, httpstatus.status[501])
+                else:
+                    self.sendResponse(msg, httpstatus.status[200])
         thread.exit()
 
     def sendResponse(self, msg, status):
+        # logger.info("Respondi o cliente com o status [%s] e msg [%s] " % (status, msg))
         response_headers = {
             'Content-Type': 'text/html; encoding=utf8',
             'Content-Length': len(msg),
             'Connection': 'close',
         }
-        response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
+        response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.iteritems())
         response_proto = 'HTTP/1.0'
         self.conexao.send('%s %s' % (response_proto, status))
         self.conexao.send(response_headers_raw)
