a495a03cf53b6fc6e065bac4ffd2fce5d2af066c
Marco Salles <marcosalles@gmail.com>
2016-09-14 18:29:42 -0300

    default config file created, not parsed

--- main.py
+++ main.py
@@ -13,34 +13,43 @@ logger = logging.getLogger(__name__)
 help = """
 HttpServer
     Usage:
-        main.py , will run on 8181 and packet size will be 32
-        main.py (-p | --port) <PORT> , must be greater than 1024
-        main.py (-l | --length) <LENGTH>, packet size must be greater than 0
+        main.py Defaults to run on localhost:8181 with a packet size of 32 bytes
+        main.py (-c | --config-file) <FILE>, must be an existing json file
     Option:
-        -p, --port
-        -l , --length
+        -c , --config-file
 """
 
+config = {
+    'port':8181,
+    'packet_length':32,
+    'allow_serve_directories':False,
+    'security':
+    {
+        'basic_auth':'root:toor',
+        'private_directories':
+        [
+            'restrito'
+        ]
+    }
+}
+
+def parse_config_file(file):
+    pass
 
 def get_args():
     parser = argparse.ArgumentParser(
-        description="A Simple HTTP server, if none where specified  will run on 8181 and packet size will be 32")
-    parser.add_argument('-p', '--port', type=int, help='Port number, must be greater than 1024',
-                        required=False, nargs='?', default=8181)
-    parser.add_argument('-l', '--length', type=int, help='Packet size, must be greater than 0',
-                        required=False, nargs='?', default=32)
-    args = parser.parse_args()
-    return args.port, args.length
-
+        description="A Simple HTTP server, if none where specified  will run on 8181 and packet size will be 32, with no password protection an will be")
+    parser.add_argument('-c','--config-file', type=str, help='Config file, must be an existing json file. An example file is provided in config.json.sample')
+    arg = parser.parse_args()
+    parse_config_file(arg.config_file)
 
 if __name__ == '__main__':
-    PORT, PACKET_LENGTH = get_args()
 
-    logger.info('Starting server at %s' % PORT)
+    logger.info('Starting server at %s' % config['port'])
 
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     # Aceita qualquer requeste de qualquer endereço, que venha na porta
-    orig = ('0.0.0.0', PORT)
+    orig = ('0.0.0.0', config['port'])
     tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcp.bind(orig)
     # Começa a ouvir
@@ -51,7 +60,7 @@ if __name__ == '__main__':
             con, client = tcp.accept()
             logger.debug("Concetado com cliente %s" % client[0])
             # Criando obj para parsear o request
-            request = httphandler(con, client, PACKET_LENGTH)
+            request = httphandler(con, client, config['packet_length'])
             # Start em uma nova thread e procesa a request
             thread.start_new_thread(request.processarRequest, ())
     except KeyboardInterrupt as e:
